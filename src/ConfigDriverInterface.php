edcbd1278aff51d6c95abe7c93d2d4af057c9d76
slexx <slexx1234@gmail.com>
2017-07-30 23:41:30 +1000

    Сделал методы статичными

--- src/ConfigDriverInterface.php
+++ src/ConfigDriverInterface.php
@@ -8,13 +8,13 @@ interface ConfigDriverInterface
      * @param string $file
      * @return array
      */
-    public function parse($file);
+    public static function parse($file);
 
     /**
      * @param string $file
      * @param array $data
      * @return array
      */
-    public function write($file, $data);
+    public static function write($file, $data);
 }
 

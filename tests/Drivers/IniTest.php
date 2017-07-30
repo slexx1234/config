16f9764825222bc706098e858694ba02a600f66a
slexx <slexx1234@gmail.com>
2017-07-31 00:46:08 +1000

    Вынес путь к файлу в константу

--- tests/Drivers/IniTest.php
+++ tests/Drivers/IniTest.php
@@ -15,15 +15,17 @@ class IniTest extends TestCase
 
     const TEST_STRING = "foo = \"bar\"\n[bar]\nfoo = \"bar\"\nbar = \"baz\"\n";
 
+    const TEST_FILE = __DIR__ . '/config.ini';
+
     public function testWrite()
     {
-        Ini::write(__DIR__ . '/config.ini', self::TEST_ARRAY);
-        $this->assertEquals(file_get_contents(__DIR__ . '/config.ini'), self::TEST_STRING);
+        Ini::write(self::TEST_FILE, self::TEST_ARRAY);
+        $this->assertEquals(file_get_contents(self::TEST_FILE), self::TEST_STRING);
     }
 
     public function testParse()
     {
-        $this->assertEquals(self::TEST_ARRAY, Ini::parse(__DIR__ . '/config.ini'));
-        unlink(__DIR__ . '/config.ini');
+        $this->assertEquals(self::TEST_ARRAY, Ini::parse(self::TEST_FILE));
+        unlink(self::TEST_FILE);
     }
 }

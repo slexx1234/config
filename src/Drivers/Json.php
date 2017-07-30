5c5c8227e0e93d2120b223ffc310b173b564cae8
slexx <slexx1234@gmail.com>
2017-07-30 23:44:01 +1000

    Написал драйвер для json файлов

---
+++ src/Drivers/Json.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Slexx\Config\Drivers;
+
+use Slexx\Config\ConfigDriverInterface;
+
+class Json implements ConfigDriverInterface
+{
+    /**
+     * @param string $file
+     * @return array
+     */
+    public static function parse($file)
+    {
+        return json_decode(file_get_contents($file), true);
+    }
+
+    /**
+     * @param string $file
+     * @param array $data
+     * @return void
+     */
+    public static function write($file, $data)
+    {
+        file_put_contents($file, json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+    }
+}
